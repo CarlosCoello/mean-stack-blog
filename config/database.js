02db6d030a85cc47bcf17071ffb768338017beb7
Carlos Coello <carlos.coello05@outlook.com>
2017-10-31 14:35:03 -0600

    database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-  uri: 'mongodb://localhost:27017/mean-stack',
+  uri: 'mongodb://carlos:carlosabc@ds243345.mlab.com:43345/heroku_jl0q256v',
   specialpassword: crypto,
   db: 'mean-stack'
 }
